50ff0bd1189769908d589a339596b6c1de0817d2
utksingh767 <767utksingh@gmail.com>
2025-06-07 08:54:02 +0530

    day2

--- course_content/Python_Day_3/Function Demos/Example1.py
+++ course_content/Python_Day_3/Function Demos/Example1.py
@@ -2,7 +2,7 @@ def myfun1():     # memory allocation happens here for the function
     print("First")
     print("Second")
     print("Third")
-myfun1()
+myfun1()   #function call
 
 print(type(myfun1))
-print(id(myfun1))
+print(id(myfun1)) #id is the address of the function
